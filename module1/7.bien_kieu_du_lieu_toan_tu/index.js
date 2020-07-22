4d97e582d056a119e73f4b762d2b75a50fbfe643
tronghuy309 <58214614+tronghuy309@users.noreply.github.com>
2020-07-22 20:07:23 +0700

    abcc

---
+++ module1/7.bien_kieu_du_lieu_toan_tu/index.js
@@ -0,0 +1,6 @@
+let h=prompt("nhap chieu cao hinh chu nhat")
+let w=prompt("nhap chieu rong cua hinh chu nhat")
+let height=parseInt(h)
+let width=parseInt(w)
+let area=height*width
+document.write("dien tich hinh chu nhat la "+area)
